d41dc7a0fc3e51e087ab8d0bbc3b4107eef0f806
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2023-06-02 21:20:43 +0200

    add safety

--- api/views.py
+++ api/views.py
@@ -4,46 +4,62 @@ from firebase_admin import db
 
 @api_view(['GET'])
 def getRoutes(request):
-    reference = db.reference("/routes/")
-    response = reference.get()
-    return Response(response)
+    try:
+        reference = db.reference("/routes/")
+        response = reference.get()
+        return Response(response)
+    except:
+        return Response('Some error')
 
 @api_view(['GET'])
 def getTimes(request):
-    reference = db.reference("/times/")
-    response = reference.get()
-    return Response(response)
+    try:
+        reference = db.reference("/times/")
+        response = reference.get()
+        return Response(response)
+    except:
+        return Response('Some error')
 
 @api_view(['GET'])
 def getTime(request, id):
-    reference = db.reference("/times/{}/".format(int(id)-1))
-    response = reference.get()
-    return Response(response)
+    try:
+        reference = db.reference("/times/{}/".format(int(id)-1))
+        response = reference.get()
+        return Response(response)
+    except:
+        return Response('Some error')
 
 @api_view(['POST'])
 def createTime(request):
     #ToDo:Validate
-    reference = db.reference("/times/")
-    response = reference.get()
-    table = []
-
     try:
-        for r in response:
-            table.append(r)
+        reference = db.reference("/times/")
+        response = reference.get()
+        table = []
+
+        try:
+            for r in response:
+                table.append(r)
+        except:
+            pass
+
+        table.append(request.data)
+        reference.set(table)
     except:
-        pass
-
-    table.append(request.data)
-    reference.set(table)
-
-    return Response()
+        return Response('Some error')
 
 @api_view(['PUT'])
 def updateTime(request,id):
-    db.reference("/times/{}/".format(int(id)-1)).update(request.data)
-    return Response()
+    try:
+        db.reference("/times/{}/".format(int(id)-1)).update(request.data)
+        return Response('Success')
+    except:
+        return Response('Some error')
 
 @api_view(['DELETE'])
 def deleteTime(request,id):
-    db.reference("/times/{}/".format(int(id)-1)).delete()
-    return Response()
+    try:
+        db.reference("/times/{}/".format(int(id)-1)).delete()
+        return Response('Success')
+    except:
+        return Response('Some error')
